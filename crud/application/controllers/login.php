1c505d2cb6bd77f47675f99c9558754bed1291eb
AirlanggaBayu <55471137+AirlanggaBayu@users.noreply.github.com>
2020-03-09 08:27:09 +0700

    belajarCI dan crud

--- crud/application/controllers/login.php
+++ crud/application/controllers/login.php
@@ -1,42 +1,45 @@
-<?php 
+<?php
 
-class Login extends CI_Controller{
+class Login extends CI_Controller
+{
 
-	function __construct(){
-		parent::__construct();		
+	function __construct()
+	{
+		parent::__construct();
 		$this->load->model('m_login');
-
 	}
 
-	function index(){
+	function index()
+	{
 		$this->load->view('v_login');
 	}
 
-	function aksi_login(){
+	function aksi_login()
+	{
 		$username = $this->input->post('username');
 		$password = $this->input->post('password');
 		$where = array(
 			'username' => $username,
 			'password' => md5($password)
-			);
-		$cek = $this->m_login->cek_login("admin",$where)->num_rows();
-		if($cek > 0){
+		);
+		$cek = $this->m_login->cek_login("admin", $where)->num_rows();
+		if ($cek > 0) {
 
 			$data_session = array(
 				'nama' => $username,
 				'status' => "login"
-				);
+			);
 
 			$this->session->set_userdata($data_session);
 
 			redirect(base_url("admin"));
-
-		}else{
+		} else {
 			echo "Username dan password salah !";
 		}
 	}
 
-	function logout(){
+	function logout()
+	{
 		$this->session->sess_destroy();
 		redirect(base_url('login'));
 	}
